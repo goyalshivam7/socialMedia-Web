1f518f7a0b66afa7d2d01b22945dc73a99b7e79e
shivamgoyal7 <goyalshivam661@gmail.com>
2020-10-13 19:16:18 +0530

    updated

--- frames.js
+++ frames.js
@@ -1,10 +1,21 @@
 function displayImageOnScreen(imageRef) {
     imageRef.getDownloadURL().then(function(downloadURL) {
         console.log(downloadURL);
-        // document.getElementById("setImageInFrames").innerHTML = "";
+
         var tickets = document.getElementById("setImageInFrames").innerHTML;
-        var frame = "<div class=\"row my-5\">";
+        var frame = "<div class=\"card w-75 mb-5\">";
+        frame += "<div class=\"row\">";
+        frame += "<div class=\"col-md-5\">";
         frame += "<img src='" + downloadURL + "' height=\"300\" width=\"300\">";
+        frame += "</div>";
+        frame += "<div class=\"col-md-7\">";
+        frame += "<div class=\"card-body\">";
+        frame += "<h5 class=\"card-title\"> Shivam goyal </h5>";
+        frame += "<p class=\"card-text\"> Shivam goyal </p>";
+        frame += "<a href=\"#\" class=\"btn btn-primary\"> Comment </a>";
+        frame += "</div>";
+        frame += "</div>";
+        frame += "</div>";
         frame += "</div>";
         tickets += frame;
 
